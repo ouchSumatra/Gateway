294ef2242fc45c86262937abcf5649b75625870f
Lem Reid <25248984+ouchSumatra@users.noreply.github.com>
2026-05-09 05:14:26 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// Global Gateway Configuration
+
+const CONFIG = {
+    appName: "Gateway",
+    version: "0.1",
+    supabaseUrl: "https://YOUR_PROJECT.supabase.co",
+    supabaseKey: "YOUR_PUBLIC_ANON_KEY",
+    ledgerTable: "ledger",
+    defaultLedgerLimit: 20
+};
+
+export default CONFIG;
